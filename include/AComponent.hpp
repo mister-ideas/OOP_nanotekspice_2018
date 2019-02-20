926019d84b788676d951b8aafe268705228c66c1
Vincent Attali <vincent.attali@epitech.eu>
2019-02-20 15:38:19 +0100

    Added setLink and dump functions

--- include/AComponent.hpp
+++ include/AComponent.hpp
@@ -18,9 +18,9 @@ namespace nts {
         public:
             virtual ~AComponent() = default;
             virtual nts::Tristate compute(std::size_t pin = 1) = 0;
-            virtual void setLink(std::size_t pin, nts::IComponent &other,
-            std::size_t otherPin) = 0;
-            virtual void dump() const = 0;
+            void setLink(std::size_t pin, nts::IComponent &other,
+            std::size_t otherPin);
+            void dump() const;
             const std::string &getName() const;
             const std::string &getType() const;
             std::vector<Pin *> getPins() const;
--- src/AComponent.cpp
+++ src/AComponent.cpp
@@ -6,8 +6,24 @@
 */
 
 #include "AComponent.hpp"
+#include "Error.hpp"
 
 namespace nts {
+    void AComponent::setLink(std::size_t pin, nts::IComponent &other, std::size_t otherPin)
+    {
+        if (pin < 1 || pin > _pins.size()
+        || dynamic_cast<AComponent *>(&other)->getPins()[otherPin - 1]->getType() == Pin::PIN_TYPE::VDD
+        || dynamic_cast<AComponent *>(&other)->getPins()[otherPin - 1]->getType() == Pin::PIN_TYPE::VSS)
+            throw Error("Link: Unknown or unlinkable pin");
+        _pins[pin - 1]->setLinkedComponent(&other);
+        _pins[pin - 1]->setLinkedPin(otherPin - 1);
+    }
+
+    void AComponent::dump() const
+    {
+        std::cout << _name << ' - ' << _type << std::endl;
+    }
+
     const std::string &AComponent::getName() const
     {
         return _name;
